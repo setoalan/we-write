d25de4fe7135edf017b6dc5472c786303c261d48
Alan Seto <alan.seto@logicdrop.com>
2014-01-24 14:49:01 -0500

    Collabrify stub in MainActivity

--- src/com/melon/wewrite/MainActivity.java
+++ src/com/melon/wewrite/MainActivity.java
@@ -6,13 +6,17 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import edu.umich.imlc.android.common.Utils;
 import edu.umich.imlc.collabrify.client.CollabrifyClient;
 import edu.umich.imlc.collabrify.client.CollabrifyListener.CollabrifyBroadcastListener;
 import edu.umich.imlc.collabrify.client.CollabrifyListener.CollabrifyCreateSessionListener;
@@ -23,7 +27,9 @@ import edu.umich.imlc.collabrify.client.CollabrifyListener.CollabrifySessionList
 import edu.umich.imlc.collabrify.client.CollabrifyParticipant;
 import edu.umich.imlc.collabrify.client.CollabrifySession;
 import edu.umich.imlc.collabrify.client.exceptions.CollabrifyException;
+import edu.umich.imlc.collabrify.client.exceptions.CollabrifyUnrecoverableException;
 import edu.umich.imlc.collabrify.client.exceptions.ConnectException;
+import edu.umich.imlc.collabrify.client.exceptions.LeaveException;
 
 public class MainActivity extends Activity implements CollabrifySessionListener, CollabrifyListSessionsListener,
 													CollabrifyBroadcastListener, CollabrifyCreateSessionListener,
@@ -72,7 +78,6 @@ public class MainActivity extends Activity implements CollabrifySessionListener,
 		});
 
 		mRedo.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				mTVUR.redo();
@@ -80,11 +85,11 @@ public class MainActivity extends Activity implements CollabrifySessionListener,
 		});
 		
 		mCreateSession.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				try {
-					myClient.createSession("sessionName", tags, null, 1, createSessionListener, sessionListener);
+					myClient.createSession("Melon Session Name", tags, null, 1, createSessionListener, sessionListener);
+					// Calls onSessionCreated
 				} catch (ConnectException e) {
 					e.printStackTrace();
 				} catch (CollabrifyException e) {
@@ -93,19 +98,33 @@ public class MainActivity extends Activity implements CollabrifySessionListener,
 			}
 		});
 		
-		mLeaveSession.setOnClickListener(new OnClickListener() {
-			
+		mJoinSession.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				
+				if (myClient.inSession())
+					return;
+				try {
+					myClient.requestSessionList(tags, listSessionsListener);
+					// Calls onReceiveSessionList
+				} catch (CollabrifyException e) {
+					e.printStackTrace();
+				}
 			}
 		});
 		
-		mJoinSession.setOnClickListener(new OnClickListener() {
-			
+		mLeaveSession.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-		
+				if (!myClient.inSession())
+					return;
+				try {
+					myClient.leaveSession(false, leaveSessionListener);
+					// Calls onDisconnect
+				} catch (LeaveException e) {
+					e.printStackTrace();
+				} catch (CollabrifyException e) {
+					e.printStackTrace();
+				}
 			}
 		});
 		
@@ -119,6 +138,11 @@ public class MainActivity extends Activity implements CollabrifySessionListener,
 		tags.add("melon");
 		
 	}
+	
+	public void doBroadcast(View v) {
+		// Stub, to be used to broadcast data automatically instead of push event
+		// Calls onBroadcastDone
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -128,30 +152,43 @@ public class MainActivity extends Activity implements CollabrifySessionListener,
 	}
 
 	@Override
-	public void onError(CollabrifyException arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onError(CollabrifyException e) {
+		if (e instanceof CollabrifyUnrecoverableException) {
+			// The client has been reset and we are no longer in a session
+			onDisconnect();
+		}
+		Log.e(TAG, "error", e);
 	}
 
 	@Override
 	public void onDisconnect() {
-		// TODO Auto-generated method stub
-		
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				showToast("Left session");
+				mCreateSession.setText(R.string.create_session);
+			}
+		});
 	}
 
 	@Override
-	public void onSessionJoined(long arg0, long arg1) {
-		// TODO Auto-generated method stub
-		
+	public void onSessionJoined(long maxOrderId, long baseFileSize) {
+		sessionName = myClient.currentSession().name();
+		sessionId = myClient.currentSession().id();
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				showToast("Session Joined");
+				mJoinSession.setText(sessionName);
+			}
+		});
 	}
 
 	@Override
 	public void onSessionCreated(final CollabrifySession session) {
-		
 		sessionId = session.id();
 		sessionName = session.name();
 		runOnUiThread(new Runnable() {
-			
 			@Override
 			public void run() {
 				showToast("Session created, id: " + session.id());
@@ -161,63 +198,99 @@ public class MainActivity extends Activity implements CollabrifySessionListener,
 	}
 
 	@Override
-	public void onBroadcastDone(byte[] arg0, long arg1, long arg2) {
-		// TODO Auto-generated method stub
-		
+	public void onBroadcastDone(final byte[] event, long orderId, long srid) {
+		showToast(new String(event) + " broadcasted");
 	}
 
 	@Override
-	public void onReceiveSessionList(List<CollabrifySession> arg0) {
-		// TODO Auto-generated method stub
+	public void onReceiveSessionList(List<CollabrifySession> sessionList) {
+		if (sessionList.isEmpty()) {
+			showToast("No session available");
+			return;
+		}
+		displaySessionList(sessionList);
+	}
+	
+	private void displaySessionList(final List<CollabrifySession> sessionList) {
+		// Create a list of session names
+		List<String> sessionNames = new ArrayList<String>();
+		for (CollabrifySession s : sessionList)
+			sessionNames.add(s.name());
 		
+		// Create a dialog to show the list of session names to the user
+		final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+		builder.setTitle("Choose Session");
+		builder.setItems(sessionNames.toArray(new String[sessionList.size()]), new DialogInterface.OnClickListener() {	
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// When the user chooses a session, join it
+				sessionId = sessionList.get(which).id();
+				try {
+					myClient.joinSession(sessionId, null, joinSessionListener, sessionListener);
+				} catch (ConnectException e) {
+					e.printStackTrace();
+				} catch (CollabrifyException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		// Display the dialog
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				builder.show();
+			}
+		});
 	}
 
 	@Override
-	public void onBaseFileReceived(File arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onBaseFileReceived(File baseFile) {
+		// UNUSED
 	}
 
 	@Override
-	public void onBaseFileUploadComplete(long arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onBaseFileUploadComplete(long baseFileSize) {
+		// UNUSED
 	}
 
 	@Override
 	public void onFurtherJoinsPrevented() {
-		// TODO Auto-generated method stub
-		
+		// UNUSED	
 	}
 
 	@Override
 	public void onParticipantJoined(CollabrifyParticipant arg0) {
-		// TODO Auto-generated method stub
-		
+		// UNUSED
 	}
 
 	@Override
 	public void onParticipantLeft(CollabrifyParticipant arg0) {
-		// TODO Auto-generated method stub
-		
+		// UNUSED
 	}
 
 	@Override
-	public void onReceiveEvent(long arg0, int arg1, String arg2, byte[] arg3,
-			long arg4) {
-		// TODO Auto-generated method stub
-		
+	public void onReceiveEvent(long orderId, int submissionRegistrationId,
+			String eventType, final byte[] data, long elapsed) {
+		Utils.printMethodName(TAG);
+		runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				Utils.printMethodName(TAG);
+				String message = new String(data);
+				// Add necessary changes to EditText view
+			}
+		});
 	}
 
 	@Override
 	public void onSessionEnd(long arg0) {
-		// TODO Auto-generated method stub
-		
+		// UNUSED
 	}
 	
 	private void showToast(final String text) {
 		runOnUiThread(new Runnable() {
-	
 			@Override
 			public void run() {
 				Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
